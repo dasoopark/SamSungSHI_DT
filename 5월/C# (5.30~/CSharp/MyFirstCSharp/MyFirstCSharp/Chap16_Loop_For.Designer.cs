f9f10d0d5e80466360d5b2d459893a42a49c67f2
Da Soo Park <ektn0514@gmail.com>
2023-06-02 16:39:10 +0900

    Chap16_Loop 반복문

--- 5월/C# (5.30~/CSharp/MyFirstCSharp/MyFirstCSharp/Chap16_Loop_For.Designer.cs
+++ 5월/C# (5.30~/CSharp/MyFirstCSharp/MyFirstCSharp/Chap16_Loop_For.Designer.cs
@@ -29,6 +29,9 @@
         private void InitializeComponent()
         {
             this.btnforsum = new System.Windows.Forms.Button();
+            this.btnArraySum = new System.Windows.Forms.Button();
+            this.txtGuGu = new System.Windows.Forms.TextBox();
+            this.GuGu_Btn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnforsum
@@ -42,20 +45,58 @@
             this.btnforsum.UseVisualStyleBackColor = true;
             this.btnforsum.Click += new System.EventHandler(this.btnforsum_Click);
             // 
+            // btnArraySum
+            // 
+            this.btnArraySum.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnArraySum.Location = new System.Drawing.Point(0, 118);
+            this.btnArraySum.Name = "btnArraySum";
+            this.btnArraySum.Size = new System.Drawing.Size(719, 80);
+            this.btnArraySum.TabIndex = 1;
+            this.btnArraySum.Text = "배열 값 더하기";
+            this.btnArraySum.UseVisualStyleBackColor = true;
+            this.btnArraySum.Click += new System.EventHandler(this.btnArraySum_Click);
+            // 
+            // txtGuGu
+            // 
+            this.txtGuGu.Dock = System.Windows.Forms.DockStyle.Top;
+            this.txtGuGu.Location = new System.Drawing.Point(0, 198);
+            this.txtGuGu.Multiline = true;
+            this.txtGuGu.Name = "txtGuGu";
+            this.txtGuGu.Size = new System.Drawing.Size(719, 81);
+            this.txtGuGu.TabIndex = 2;
+            // 
+            // GuGu_Btn
+            // 
+            this.GuGu_Btn.Dock = System.Windows.Forms.DockStyle.Top;
+            this.GuGu_Btn.Location = new System.Drawing.Point(0, 279);
+            this.GuGu_Btn.Name = "GuGu_Btn";
+            this.GuGu_Btn.Size = new System.Drawing.Size(719, 23);
+            this.GuGu_Btn.TabIndex = 3;
+            this.GuGu_Btn.Text = "구구단 표현하기 2~9";
+            this.GuGu_Btn.UseVisualStyleBackColor = true;
+            this.GuGu_Btn.Click += new System.EventHandler(this.GuGu_Btn_Click);
+            // 
             // Chap16_Loop_For
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(719, 365);
+            this.Controls.Add(this.GuGu_Btn);
+            this.Controls.Add(this.txtGuGu);
+            this.Controls.Add(this.btnArraySum);
             this.Controls.Add(this.btnforsum);
             this.Name = "Chap16_Loop_For";
             this.Text = "반복문 For";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btnforsum;
+        private System.Windows.Forms.Button btnArraySum;
+        private System.Windows.Forms.TextBox txtGuGu;
+        private System.Windows.Forms.Button GuGu_Btn;
     }
 }
--- 5월/C# (5.30~/CSharp/MyFirstCSharp/MyFirstCSharp/Chap16_Loop_For.cs
+++ 5월/C# (5.30~/CSharp/MyFirstCSharp/MyFirstCSharp/Chap16_Loop_For.cs
@@ -12,6 +12,8 @@ namespace MyFirstCSharp
 {
     public partial class Chap16_Loop_For : Form
     {
+        //반복문
+        //특정한 조건을 만족 할 때 까지 반복하여 동일한 로직을 실행하는 문법
         public Chap16_Loop_For()
         {
             InitializeComponent();
@@ -19,7 +21,53 @@ namespace MyFirstCSharp
 
         private void btnforsum_Click(object sender, EventArgs e)
         {
+            //1부터 100까지 합하는 로직.
 
+            // 1. 누적 값이 저장되는 변수.
+            int iSumValue = 0;
+
+            // 1 부터 100까지 반복
+            // i: 반복하면서 조건을 만족시킬 변수
+            // 반복문을 종료 할 수 있는 변수
+            // int i = 0; : int형 변수 i가 0이라는 값을 가지고 시작.
+            // : i <= 100 = 참인 조건 인지 판단하는 구문
+            // : i++ = 후위 증가 연산자.
+            for (int i =0; i <=100; i++)
+            {
+                iSumValue += i;
+            }
+            MessageBox.Show(iSumValue.ToString());
+        }
+
+        private void btnArraySum_Click(object sender, EventArgs e)
+        {
+            // 배열 의 데이터 수 만큼 for 문으로 반복 후 배열의 값 더하기.
+            // 배열에 포함된 데이터의 갯수 및 값을 모른다고 가정.
+            int[] ivalues = { 10, 20, 30, 40, 50, 50, 20 };
+                             //0  1   2   3   4   5   6
+            int iSumValue = 0; //배열의 모든 값을 더해서 누적 시킬 변수.
+            
+            //for문을 통하여 ivalues 배열에 있는 모든 값을 더하기.
+            //배열의 주소 값으로 사용될 i
+            for(int i = 0; i<ivalues.Length; i++)
+            {
+                iSumValue += ivalues[i];
+            }
+            MessageBox.Show(iSumValue.ToString());
+
+          
+        }
+
+        private void GuGu_Btn_Click(object sender, EventArgs e)
+        {
+            for(int i = 2; i<=9; i++)
+            {
+                for(int j = 1; j<=9; j++)
+                {
+                    txtGuGu.Text += $"{i} * {j} = {i * j}\r\n";
+
+                }
+            }
         }
     }
 }
