625b311240ef45fa23e8f8d7c8c8314016ed9d60
Da Soo Park <ektn0514@gmail.com>
2023-06-07 09:09:00 +0900

    Chap19_Foreach

--- 5월/C# (5.30~/CSharp/MyFirstCSharp/MyFirstCSharp/Chap19_Foreach_Test[Foreach 예제].cs
+++ 5월/C# (5.30~/CSharp/MyFirstCSharp/MyFirstCSharp/Chap19_Foreach_Test[Foreach 예제].cs
@@ -19,19 +19,29 @@ namespace MyFirstCSharp
 
         private void button1_Click(object sender, EventArgs e)
         {
-            
+            //2차원 배열을 Foreach로 텍스트박스에 표현하기.
+
+            //1. 배열의 생성
             int[,] ivalues = new int[3,4] { { 1,2,3,4},
                                              {5,6,7, 8 },
                                              {9,10,11,12 } };
 
-            int columns = ivalues.GetLength(0);
-            MessageBox.Show(columns.ToString());
-            
-             foreach (int i in ivalues)
+            //2차원 배열의 열의 수를 구함 
+            int iArrayColumnCNT = ivalues.GetLength(1); 
+            int iColumnCnt = 0; //허리할 데이터추출의 개수가 열의 개수와 일치하는지 확인
+
+
+            foreach (int i in ivalues)
               {
-                  txtMatrix.Text += i.ToString() + " "; 
+                ++iColumnCnt; //열의 cnt 증가
+                txtMatrix.Text += i.ToString() + " "; //현재 추출한 데이터를 표현
+                if(iColumnCnt == iArrayColumnCNT)
+                {
+                    txtMatrix.Text += "\r\n";
+                    iColumnCnt = 0;
+                }
               }
-              
+             
            
             
         }
