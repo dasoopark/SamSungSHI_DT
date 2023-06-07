2fb7f9a55cd73a1926d5aee5d55f79ecd71d38db
Da Soo Park <ektn0514@gmail.com>
2023-06-07 17:19:20 +0900

    Chap20 예제

---
+++ 5월/C# (5.30~/CSharp/MyFirstCSharp/MyFirstCSharp/Chap20_MiddleTest_04.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace MyFirstCSharp
+{
+    partial class Chap20_MiddleTest_04
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.btnfindResult = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label1.Location = new System.Drawing.Point(53, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(382, 119);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "아래 문자열에 포함된 수를 정수 배열로 만들고,\r\n\r\n낮은 수 부터 중복되는 첫번째 값과 \r\n\r\n세번째 값을 메세지 박스로 나타내세요.\r\n\r\n *" +
+    " 배열을 생성할 때 에 아래 lblTitle의 내용을 받아서 배열로 만들 것.\r\n";
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lblTitle.Location = new System.Drawing.Point(54, 185);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(247, 20);
+            this.lblTitle.TabIndex = 1;
+            this.lblTitle.Text = "{1,2,13,15,17,23,8,15,8,19,3,8,13}";
+            // 
+            // btnfindResult
+            // 
+            this.btnfindResult.Location = new System.Drawing.Point(94, 224);
+            this.btnfindResult.Name = "btnfindResult";
+            this.btnfindResult.Size = new System.Drawing.Size(207, 81);
+            this.btnfindResult.TabIndex = 2;
+            this.btnfindResult.Text = "중복값찾기";
+            this.btnfindResult.UseVisualStyleBackColor = true;
+            this.btnfindResult.Click += new System.EventHandler(this.btnfindResult_Click);
+            // 
+            // Chap20_MiddleTest_04
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnfindResult);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.label1);
+            this.Name = "Chap20_MiddleTest_04";
+            this.Text = "Chap20_MiddleTest_04";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Button btnfindResult;
+    }
+}
---
+++ 5월/C# (5.30~/CSharp/MyFirstCSharp/MyFirstCSharp/Chap20_MiddleTest_04.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyFirstCSharp
+{
+    public partial class Chap20_MiddleTest_04 : Form
+    {
+        public Chap20_MiddleTest_04()
+        {
+            InitializeComponent();
+        }
+
+        private void btnfindResult_Click(object sender, EventArgs e)
+        {
+
+            // 라벨의 텍스트를 숫자로 받아오기.
+            
+            //타이틀의 문자를 숫자로 변환 하도록 문자 배열에 담기.
+            string sTitle = lblTitle.Text.Replace(" ", "")
+                                         .Replace("{", "")
+                                         .Replace("}", "");
+            string[] stitles = sTitle.Split(',');
+
+
+            //대상 문자 배열에 있는 값을 숫자 배열로 등록하기.
+            int[] iValues = new int[stitles.Length];
+            for(int i = 0; i<stitles.Length;i++)
+            {
+                iValues[i] = int.Parse(stitles[i]);
+            }
+
+            //오름차순 정렬
+            // 1 2 3 8 8 8 13 13 15 15 17 19 23 
+            Array.Sort(iValues);
+
+            string sResult = string.Empty; //첫번째, 세번째 중복값을 담을 변수.
+            int iFindCount = 0; //중복값을 찾은 횟수.
+            int iFindValue = 0; //이전에 비교한 값을 담을 변수.
+
+            // i: 정수 배열의 index
+            for (int i = 0; i < iValues.Length; i++)
+            {
+                // 중복 되는 숫자인지 판단.
+                if (iValues[i] == iValues[i + 1])
+                {
+                    if (iValues[i]==iFindValue) //현재값이 이전값이랑 같을경우
+                    {
+                        continue;
+                    }
+                    ++iFindCount; //중복값을 찾았을 경우 1 증가.
+
+
+                    // 두번째 중복 일경우 다음 값을 비교.
+                    if(iFindCount == 2)
+                    {
+                        continue;
+                    }
+
+                    //첫번째 중복 과 세번째 중복 
+                    sResult += iValues[i].ToString()+" ";
+                    if(iFindCount == 3) //세번째 중복을 찾았을 경우 반복문 종료.
+                    {
+                        break;
+                    }
+                    //다음 For문이 시작되기 전에 현재 기준값을 이전 값으로 등록을 해줌.
+                    //등록 후 for문 증가
+                    iFindValue = iValues[i];
+                }
+            }
+            MessageBox.Show($"첫번째와 세번째 중복되는 값은 {sResult}입니다.");
+        }
+    }
+}
